6024ced119dbbe37653363dd88c7c9ac1bac85a1
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-11-01 23:05:54 +0000

    Odd Even Commit

---
+++ EvenOddNumber.java
@@ -0,0 +1,18 @@
+package evenoddnumber;
+
+public class EvenOddNumber {
+
+    public static void main(String[] args){
+
+        int num = 10;
+        if ( num %2 == 0 ){
+
+            System.out.println ( num + " is EVEN ");
+        }
+          else {
+
+              System.out.println ( num + " is ODD ");
+        }
+
+    }
+}
